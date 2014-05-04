a080a4041806567cd4f0336961b62de4adc2f53a
Someil <chaugn_c@etna-alternance.net>
2014-05-04 21:21:43 +0200

    add full bootstrap theme
    création d'un thème compatible full bootstrap
    -article
    -navigation
    -caroussel (fixe en cours de modif)

--- templates/opendtp/index.php
+++ templates/opendtp/index.php
@@ -29,6 +29,7 @@ $doc->addScript('templates/' .$this->template. '/js/main.js');
 
 // Add Stylesheets
 $doc->addStyleSheet('templates/'.$this->template.'/css/bootstrap.min.css');
+$doc->addStyleSheet('templates/'.$this->template.'/css/bootstrap-theme.css');
 $doc->addStyleSheet('templates/'.$this->template.'/css/template.css');
 
 $user = JFactory::getUser();
@@ -56,19 +57,6 @@ else
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <jdoc:include type="head" />
-    <?php
-    if ($this->params->get('googleFont'))
-	{
-	?>
-		<link href='//fonts.googleapis.com/css?family=<?php echo $this->params->get('googleFontName');?>' rel='stylesheet' type='text/css' />
-		<style type="text/css">
-			h1,h2,h3,h4,h5,h6,.site-title{
-				font-family: '<?php echo str_replace('+', ' ', $this->params->get('googleFontName'));?>', sans-serif;
-			}
-		</style>
-	<?php
-	}
-    ?>
 </head>
 <body class="site <?php echo $option
 	. ' view-' . $view
@@ -77,58 +65,92 @@ else
 	. ($itemid ? ' itemid-' . $itemid : '')
 	. ($params->get('fluidContainer') ? ' fluid' : '');
 ?>">
-    <div id="container" class="main">
-        <div class="row">
-        <div class="col-md-12">
-            <div class="row" id ="header">
-                <div id="logo-events" class="col-md-12" role="banner">
-                    <h1 class="logo"><a href="./" title="OpenDTP"><img id="logo" src="<?php echo $templatePath;?>/images/logo.png" /></a></h1>
+    <?php if ($this->countModules('position-1')) : ?>
+    <div class="navbar-wrapper">
+        <div class="container">
+            <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">OpenDTP</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <jdoc:include type="modules" name="position-1" style="none" />
                 </div>
             </div>
-            <div class="wrapper">
-                <?php if ($this->countModules('position-1')) : ?>
-                <nav class="row navigation" role="navigation">
-                    <div class="col-md-12 bwhite">
-                        <div class="col-md-9" >
-                            <jdoc:include type="modules" name="position-1" style="none" />
-                        </div>
-                        <div id="searchbox" class="col-md-3" >
-                            <jdoc:include type="modules" name="position-0" style="none" />
-                        </div>
-                    <div>
-                </nav>
-                <?php endif; ?>
-                <?php if ($this->countModules('position-3')) : ?>
-                    <div class="row slideshow">
-                        <div class="col-md-12">
-                        <jdoc:include type="modules" name="position-3" style="xhtml" />
-                        </div>
-                    </div>
-                <?php endif; ?>
-                <div class="row content">
-                    <div class="col-md-9">
-                        <jdoc:include type="message" />
-                        <jdoc:include type="component" />
-                        <jdoc:include type="modules" name="position-2" style="none" />
-                    </div>
-                    <?php if ($this->countModules('position-3')) : ?>
-                    <div class="col-md-3 sidebar-right">
-                        <!-- Begin Right Sidebar -->
-                        <jdoc:include type="modules" name="position-7" style="well" />
-                        <!-- End Right Sidebar -->
-                    </div>
-                    <?php endif; ?>
-                </div>
-            </div>
-            <?php if ($this->countModules('position-3')) : ?>
-            <div id="footer" class="row">
-                <div class="col-md-12" role="footer">
-                    <jdoc:include type="modules" name="footer" style="none" />
-                </div>
-            </div>
-            <?php endif; ?>
         </div>
-        <jdoc:include type="modules" name="debug" style="none" />
+    </div>
+    <?php endif; ?>
+    
+    <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="item active">
+          <img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" >
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Example headline.</h1>
+              <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+            </div>
+          </div>
         </div>
+        <div class="item">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>One more for good measure.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+    </div><!-- /.carousel -->
+    
+    <?php // Carousel
+        if ($this->countModules('position-3') && false) : ?>
+        <div class="row slideshow">
+           <?php /* <div class="col-md-12">
+            <jdoc:include type="modules" name="position-3" style="xhtml" />
+            </div> */?>
+        </div>
+    <?php endif; ?>
+    <div class="container article">
+        <div class="wrapper">
+            
+            <jdoc:include type="message" />
+            <jdoc:include type="component" />
+            <!--<jdoc:include type="modules" name="position-2" style="none" />-->
+        </div>
+        <?php if ($this->countModules('position-3')) : ?>
+        <div id="footer" class="row">
+            <div class="col-md-12" role="footer">
+                <jdoc:include type="modules" name="footer" style="none" />
+            </div>
+        </div>
+        <?php endif; ?>
+        <!--<jdoc:include type="modules" name="debug" style="none" />-->
     </div>
 </body>
